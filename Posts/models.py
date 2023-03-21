e200b8d92a916b51f63d4fd2dd232939a1edcf5f
loranYousef <loganyousef@gmail.com>
2023-03-21 17:08:42 +0100

    add taggit relation static media

--- Posts/models.py
+++ Posts/models.py
@@ -1,6 +1,21 @@
 from django.db import models
+from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content= models.CharField(max_length=1000)
+    image = models.ImageField(upload_to='posts/')
+    author= models.ForeignKey(User,related_name='post_author',on_delete=models.CASCADE)
+    tags = TaggableManager()
+    
+    
+    
+    def __str__(self):
+        return self.title 
+    
+    
+    
+   
+ 
--- myblog/settings.py
+++ myblog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "Posts",
+    "taggit",
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    
+]
+MEDIA_URL= 'media/'
+MEDIA_ROOT= BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
