83580cb8fe98d35b1d9bff908050e555ec4e517f
loranYousef <loganyousef@gmail.com>
2023-03-27 19:11:45 +0200

    studying crud operation

--- Posts/models.py
+++ Posts/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=15000)
+    image = models.ImageField(upload_to='posts/')
+    author = models.ForeignKey(User,related_name= 'post_author', on_delete=models.CASCADE)
+    tags = TaggableManager()
 
 
     def __str__(self):
