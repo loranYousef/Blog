f2a1393ab5dc0492f76f2e06ad88f169ee620911
loranYousef <loganyousef@gmail.com>
2023-09-19 22:45:33 +0200

    add register class

--- Posts/admin.py
+++ Posts/admin.py
@@ -8,5 +8,5 @@ class PostAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
     summernote_fields = '__all__'
 
 admin.site.register(Kontakt)
-# admin.site.register(Post,PostAdmin)
+ admin.site.register(Post,PostAdmin)
 # admin.site.register(Category)
